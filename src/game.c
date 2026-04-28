ab47e3d850b388a3b5e60eee6f5c3dbbde360392
jpires-n <luckyjoey13@gmail.com>
2026-04-28 20:33:44 -0300

    refactor: define in game properties the width and height

--- src/game.c
+++ src/game.c
@@ -6,7 +6,7 @@
 /*   By: jpires-n <jpires-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 19:07:14 by jpires-n          #+#    #+#             */
-/*   Updated: 2026/04/28 19:08:08 by jpires-n         ###   ########.fr       */
+/*   Updated: 2026/04/28 20:31:27 by jpires-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 void	game_init(t_game *game)
 {
+	game->width = 1280;
+	game->height = 720;
 	game->mlx = mlx_init();
-	game->win = mlx_new_window(game->mlx, 1280, 720, "cub3d");
-	game->img = mlx_new_image(game->mlx, 1280, 720);
+	game->win = mlx_new_window(game->mlx, game->width, game->height, "cub3d");
+	game->img = mlx_new_image(game->mlx, game->width, game->height);
 	game->img_data = mlx_get_data_addr(
 			game->img, &game->bpp, &game->line_len, &game->endian);
 }
