4d74a595de86ae1d65f261a8513773001df9f0a1
jpires-n <luckyjoey13@gmail.com>
2026-04-28 20:32:40 -0300

    fix: update put_pixel function to handle pixel placement correctly
    Finds the pixel address and then fill it with the color from the param

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: jpires-n <jpires-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 19:09:14 by jpires-n          #+#    #+#             */
-/*   Updated: 2026/04/28 19:10:47 by jpires-n         ###   ########.fr       */
+/*   Updated: 2026/04/28 20:21:54 by jpires-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 
 void	put_pixel(t_game *game, int x, int y, int color)
 {
+	char	*dst;
+	int		offset;
 
+	offset = (y * game->line_len) + (x * (game->bpp / 8));
+	if (x < 0 || x >= game->width || y < 0 || y >= game->height)
+		return ;
+	dst = game->img_data + offset;
+	*(unsigned int *)dst = color;
 }
 
