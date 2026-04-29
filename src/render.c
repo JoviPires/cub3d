d6f33d3947e7a109ec450fe1a1871c6b440079ce
jpires-n <luckyjoey13@gmail.com>
2026-04-28 21:28:26 -0300

    feat: implement rendering logic for ceiling and floor colors
    Added a new render function to handle the rendering of the game scene, distinguishing between ceiling and floor colors based on the pixel's vertical position. Introduced a helper function to convert RGB values to an integer format for color representation.

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: jpires-n <jpires-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 19:09:14 by jpires-n          #+#    #+#             */
-/*   Updated: 2026/04/28 20:21:54 by jpires-n         ###   ########.fr       */
+/*   Updated: 2026/04/28 21:25:21 by jpires-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,39 @@ void	put_pixel(t_game *game, int x, int y, int color)
 	char	*dst;
 	int		offset;
 
-	offset = (y * game->line_len) + (x * (game->bpp / 8));
 	if (x < 0 || x >= game->width || y < 0 || y >= game->height)
 		return ;
+	offset = (y * game->line_len) + (x * (game->bpp / 8));
 	dst = game->img_data + offset;
 	*(unsigned int *)dst = color;
 }
 
+static int	rgb_to_int(t_color color)
+{
+	return (color.r << 16 | color.g << 8 | color.b);
+}
+
+int	render(t_game *game)
+{
+	int	x;
+	int	y;
+	int	ceiling_color;
+	int	floor_color;
+
+	ceiling_color = rgb_to_int(game->scene.ceiling);
+	floor_color = rgb_to_int(game->scene.floor);
+	y = -1;
+	while (++y < game->height)
+	{
+		x = -1;
+		while (++x < game->width)
+		{
+			if (y < game->height / 2)
+				put_pixel(game, x, y, ceiling_color);
+			else
+				put_pixel(game, x, y, floor_color);
+		}
+	}
+	mlx_put_image_to_window(game->mlx, game->win, game->img, 0, 0);
+	return (0);
+}
