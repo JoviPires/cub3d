eb6f362562c1acce85aa4ad2552e70400a3bed61
jpires-n <luckyjoey13@gmail.com>
2026-04-29 21:07:27 -0300

    feat: enhance rendering logic with wall color determination and column drawing
    Now render is an orchestrator calling for other functions.

--- src/render.c
+++ src/render.c
@@ -29,27 +29,45 @@ static int	rgb_to_int(t_color color)
 	return (color.r << 16 | color.g << 8 | color.b);
 }
 
+static int	get_wall_color(t_ray *ray)
+{
+	if (ray->side == 0 && ray->ray_dir_x > 0)
+		return (0xCC3333);
+	if (ray->side == 0 && ray->ray_dir_x <= 0)
+		return (0x33CC33);
+	if (ray->side == 1 && ray->ray_dir_y > 0)
+		return (0x3333CC);
+	return (0xCCCC33);
+}
+
+void	draw_column(t_game *game, t_ray *ray, int x)
+{
+	int	ceil_c;
+	int	floor_c;
+	int	wall_c;
+	int	y;
+
+	ceil_c = rgb_to_int(game->scene.ceiling);
+	floor_c = rgb_to_int(game->scene.floor);
+	wall_c = get_wall_color(ray);
+	y = -1;
+	while (++y < ray->draw_start)
+		put_pixel(game, x, y, ceil_c);
+	while (y <= ray->draw_end)
+	{
+		put_pixel(game, x, y, wall_c);
+		y++;
+	}
+	while (y < game->height)
+	{
+		put_pixel(game, x, y, floor_c);
+		y++;
+	}
+}
+
 int	render(t_game *game)
 {
-	int	x;
-	int	y;
-	int	ceiling_color;
-	int	floor_color;
-
-	ceiling_color = rgb_to_int(game->scene.ceiling);
-	floor_color = rgb_to_int(game->scene.floor);
-	y = -1;
-	while (++y < game->height)
-	{
-		x = -1;
-		while (++x < game->width)
-		{
-			if (y < game->height / 2)
-				put_pixel(game, x, y, ceiling_color);
-			else
-				put_pixel(game, x, y, floor_color);
-		}
-	}
+	cast_rays(game);
 	mlx_put_image_to_window(game->mlx, game->win, game->img, 0, 0);
 	return (0);
 }
